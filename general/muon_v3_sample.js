bbe49f365c5cbd2f8bb8b875f3bc3c6ecdfa1af4
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-08-23 00:25:17 +0430

    MuonV3 sample app

---
+++ general/muon_v3_sample.js
@@ -0,0 +1,36 @@
+const {soliditySha3} = MuonAppUtils
+
+module.exports = {
+  APP_NAME: 'muon_v3_sample',
+
+  onRequest: async (request) => {
+    switch (method) {
+      case 'test':
+        return {
+          appId: this.APP_ID,
+          testParam: 100, // uint256
+        }
+
+      default:
+        throw { message: `Unknown method ${params}` }
+    }
+  },
+
+  hashRequestResult: (request, result) => {
+    let { method } = request
+    switch (method) {
+      case 'test':
+        let { testParam } = result;
+        return soliditySha3([
+          { type: 'uint32', value: this.APP_ID },
+          // request.hash is the reqId that goes to the chain
+          // it is deterministic and can be signed
+          { type: 'bytes', value: request.hash},
+          { type: 'uint256', value: testParam}
+        ])
+
+      default:
+        break
+    }
+  }
+}
