5aac85aafe154ca21b9aa93edeef7dbc3392927b
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-08-24 11:59:13 +0430

    v3 sample

--- general/muon_v3_sample.js
+++ general/muon_v3_sample.js
@@ -3,12 +3,12 @@ const {soliditySha3} = MuonAppUtils
 module.exports = {
   APP_NAME: 'muon_v3_sample',
 
-  onRequest: async (request) => {
+  onRequest: async function(request){
     let { method } = request
     switch (method) {
       case 'test':
         return {
-          testParam: 100, // uint256
+          testParam: "100", // uint256
         }
 
       default:
@@ -16,19 +16,41 @@ module.exports = {
     }
   },
 
-  hashRequestResult: function(request, result){
-    let { method } = request
+  // Deprecated in favor of signParams
+
+  // hashRequestResult: function(request, result){
+  //   let { method } = request
+  //   switch (method) {
+  //     case 'test':
+  //       let { testParam } = result;
+  //       console.log(this.APP_ID, request.hash, testParam)
+  //       return soliditySha3([
+  //         { type: 'uint32', value: this.APP_ID },
+  //         // request.hash is the reqId that goes to the chain
+  //         // it is deterministic and can be signed
+  //         { type: 'bytes', value: request.hash},
+  //         { type: 'uint256', value: testParam}
+  //       ])
+
+  //     default:
+  //       break
+  //   }
+  // }
+
+  /**
+   * List of the parameters that need to be signed. 
+   * APP_ID, reqId will be added by the
+   * Muon Core and [APP_ID, reqId, … signParams]
+   * should be verified on chain.
+   */
+  signParams: function(request, result){
+    let { method } = request;
+    let { testParam } = result;
     switch (method) {
       case 'test':
-        let { testParam } = result;
-        return soliditySha3([
-          { type: 'uint32', value: this.APP_ID },
-          // request.hash is the reqId that goes to the chain
-          // it is deterministic and can be signed
-          { type: 'bytes', value: request.hash},
+        return [
           { type: 'uint256', value: testParam}
-        ])
-
+        ]
       default:
         break
     }
