c30a1cfae43b34ea35a943981ffd2c03b03ce984
smrm-dev <mrmousavi78@gmail.com>
2022-08-29 12:36:35 +0430

    Import constants from app

--- general_test/price_feed_test.js
+++ general_test/price_feed_test.js
@@ -1,12 +1,31 @@
 require('dotenv').config({ path: './dev-chain/dev-node-1.env' })
 require('../../src/core/global')
-const { } = require('../general/price_feed')
+const assert = require('assert')
+const { dynamicExtend } = require('../../src/core/utils')
+const PriceFeedApp = dynamicExtend(
+    class { },
+    require('../general/price_feed')
+)
 
 const Red = "\x1b[31m"
 const Green = "\x1b[32m"
 const Blue = "\x1b[34m"
 const Reset = "\x1b[0m"
 
+const app = new PriceFeedApp()
+const {
+    CHAINS,
+    networksWeb3,
+    networksBlocks,
+    THRESHOLD,
+    PRICE_TOLERANCE,
+    FUSE_PRICE_TOLERANCE,
+    Q112,
+    ETH,
+    UNISWAPV2_PAIR_ABI,
+    BN,
+    toBaseUnit,
+} = app
 
 const testGetSeed = async () => {
     throw 'error'    
@@ -58,10 +77,10 @@ const tests = [
 ]
 
 tests.forEach((test) => {
-   test() 
-    .then(() => console.log(`${Green}Passed ${Blue}${test.name}`))
-    .catch((error) => {
-        console.log(`${Red}Failed ${Blue}${test.name}${Reset}`)
-        // console.log('\t', error)
-    })
+    test()
+        .then(() => console.log(`${Green}Passed ${Blue}${test.name}`))
+        .catch((error) => {
+            console.log(`${Red}Failed ${Blue}${test.name}${Reset}`)
+            console.log('\t', error.message)
+        })
 })
