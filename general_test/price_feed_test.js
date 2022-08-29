a3599a91e33b781fec5b56ebe5b2f795e9331740
smrm-dev <mrmousavi78@gmail.com>
2022-08-29 12:39:43 +0430

    Implement ```testGetSyncEvents```

--- general_test/price_feed_test.js
+++ general_test/price_feed_test.js
@@ -31,8 +31,35 @@ const testGetSeed = async () => {
     throw 'error'    
 }
 
-const testGetSyncEvents = async() => {
-    throw 'error'
+const testGetSyncEvents = async () => {
+    const chainId = 1
+    const seedBlockNumber = 14506359
+    const pairAddress = '0x328dfd0139e26cb0fef7b0742b49b0fe4325f821'
+
+    const w3 = networksWeb3[chainId]
+    const pair = new w3.eth.Contract(UNISWAPV2_PAIR_ABI, pairAddress)
+    const options = {
+        fromBlock: seedBlockNumber + 1,
+        toBlock: seedBlockNumber + networksBlocks[chainId]['seed']
+    }
+    const syncEvents = await pair.getPastEvents("Sync", options)
+
+    const syncEventsMap = await app.getSyncEvents(chainId, seedBlockNumber, pairAddress)
+    const numberOfEvents = Object.keys(syncEventsMap).length
+
+    let lastEvent = {
+        blockNumber: 0
+    }
+    let counter = 0
+    syncEvents.reverse().forEach((event) => {
+        if (lastEvent.blockNumber != event.blockNumber) {
+            assert(syncEventsMap[event.blockNumber].transactionHash == event.transactionHash, `transactionIndex ${event.transactionHash}, ${syncEventsMap[event.blockNumber].transactionHash}`)
+            assert(syncEventsMap[event.blockNumber].logIndex == event.logIndex, `logIndex ${event.logIndex}, ${syncEventsMap[event.blockNumber].logIndex}, ${event.blockNumber}`)
+            lastEvent = event
+            counter++
+        }
+    })
+    assert(counter == numberOfEvents, `Difference in number of events ${counter} ${numberOfEvents}`)
 }
 
 const testCreatePrice = async() => {
