3283845e9965def2645d9c965a2c4274ac02129f
smrm-dev <mrmousavi78@gmail.com>
2022-08-31 10:48:33 +0430

    Paste ```testGetSyncEvents``` implementation in the new test file

--- general_test/price_feed.test.js
+++ general_test/price_feed.test.js
@@ -1,6 +1,6 @@
-
 require('dotenv').config({ path: './dev-chain/dev-node-1.env' })
 require('../../src/core/global')
+const assert = require('assert')
 
 const { dynamicExtend } = require('../../src/core/utils')
 const PriceFeedApp = dynamicExtend(
@@ -22,10 +22,63 @@ const {
     toBaseUnit,
 } = app
 
+const RED = "\x1b[31m"
+const GREEN = "\x1b[32m"
+const BLUE = "\x1b[34m"
+const CYAN = "\x1b[36m"
+const RESET = "\x1b[0m"
+function injectColor(color, text) {
+    return color + text
+}
+
 
 describe('Price Feed app unit test', () => {
     it('Test getSyncEvents', async () => {
-        expect(false).toBe(true)
+        const chainId = 1
+        const seedBlockNumber = 14506359
+        const pairAddress = '0x328dfd0139e26cb0fef7b0742b49b0fe4325f821'
+
+        const w3 = networksWeb3[chainId]
+        const pair = new w3.eth.Contract(UNISWAPV2_PAIR_ABI, pairAddress)
+        const options = {
+            fromBlock: seedBlockNumber + 1,
+            toBlock: seedBlockNumber + networksBlocks[chainId]['seed']
+        }
+        const syncEvents = await pair.getPastEvents("Sync", options)
+
+        const syncEventsMap = await app.getSyncEvents(chainId, seedBlockNumber, pairAddress)
+        const numberOfEvents = Object.keys(syncEventsMap).length
+
+        let lastEvent = {
+            blockNumber: 0
+        }
+        let counter = 0
+        syncEvents.reverse().forEach((event) => {
+            if (lastEvent.blockNumber != event.blockNumber) {
+                const result = syncEventsMap[event.blockNumber]
+                assert(
+                    result.transactionHash == event.transactionHash,
+                    `${injectColor(BLUE, 'Transaction hash must be the same')}
+                    Expected: ${injectColor(GREEN, event.transactionHash)}
+                    Received: ${injectColor(RED, result.transactionHash)}`
+                )
+                assert(
+                    result.logIndex == event.logIndex,
+                    `${injectColor(BLUE, 'Log index must be the same')}
+                    Expected: ${injectColor(GREEN, event.logIndex)}
+                    Received: ${injectColor(RED, result.logIndex)}
+                    BlockNumber: ${injectColor(CYAN, event.blockNumber)}`
+                )
+                lastEvent = event
+                counter++
+            }
+        })
+        assert(
+            counter == numberOfEvents,
+            `${injectColor(BLUE, 'Number of events must be the same')}
+            Expected: ${injectColor(GREEN, counter)}
+            Received: ${injectColor(RED, numberOfEvents)} `
+        )
     })
     it('Test createPrices', async () => {
         expect(false).toBe(true)
