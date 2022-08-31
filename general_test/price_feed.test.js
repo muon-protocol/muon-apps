38cf870c9165cc318215100b3052a1873bf3e657
smrm-dev <mrmousavi78@gmail.com>
2022-08-31 15:39:37 +0430

    Implement createPrices test in the new test file Delete old test file

--- general_test/price_feed.test.js
+++ general_test/price_feed.test.js
@@ -80,9 +80,77 @@ describe('Price Feed app unit test', () => {
             Received: ${injectColor(RED, numberOfEvents)} `
         )
     })
+
     it('Test createPrices', async () => {
-        expect(false).toBe(true)
+        const chainId = 250
+        const seed = {
+            price0: new BN('553937927341650325448601038471856'),
+            blockNumber: 14506224
+        }
+        const blocksWithEvent = {
+            1: '14506230',
+            2: '14506280'
+        }
+        const syncEventsMap = {
+            [blocksWithEvent[1]]: {
+                returnValues: {
+                    reserve0: '399657354506030558473',
+                    reserve1: '50540763706853586504',
+                }
+            },
+
+            [blocksWithEvent[2]]: {
+                returnValues: {
+                    reserve0: '407073280527237345605',
+                    reserve1: '49622740867094652888',
+                }
+            }
+        }
+
+        const prices = app.createPrices(chainId, seed, syncEventsMap)
+
+        assert(
+            prices.length == networksBlocks[chainId]['seed'] + 1,
+            `${injectColor(BLUE, 'Prices array has invalid length')}
+            Expected: ${injectColor(GREEN, networksBlocks[chainId]['seed'] + 1)}
+            Received: ${injectColor(RED, prices.length)}`
+        )
+        assert(
+            prices[0].eq(seed.price0),
+            `${injectColor(BLUE, 'Zero index must be seed price')}
+            Expected: ${injectColor(GREEN, seed.price0)} 
+            Received: ${injectColor(RED, prices[0])}
+            `
+        )
+        let gaps = []
+        // calculate gaps between blocks with sync event
+        Object.values(blocksWithEvent).forEach((block, index) => gaps[index] = block - seed.blockNumber)
+        // check if for each gap the correct price has been set
+        let result
+        let lastGap = 0
+        let expectedPrice = seed.price0
+        gaps.forEach((gap, j) => {
+            // create a set of prices for each gap
+            result = new Set(prices.slice(lastGap, gap))
+            // check if each gap has one price
+            assert(
+                result.size == 1,
+                `${injectColor(BLUE, 'Multiple price for a gap')}`
+            )
+            // check if gap price is correct
+            assert(
+                [...result][0].eq(expectedPrice),
+                `${injectColor(BLUE, 'Wrong price set for block')}
+                Expected: ${injectColor(GREEN, expectedPrice)}
+                Received: ${injectColor(RED, [...result][0])})`
+            )
+
+            const { reserve0, reserve1 } = syncEventsMap[blocksWithEvent[j + 1]].returnValues
+            expectedPrice = app.calculateInstantPrice(reserve0, reserve1)
+            lastGap = gap
+        })
     })
+
     it('Test removeOutlierZScore', async () => {
         expect(false).toBe(true)
     })
