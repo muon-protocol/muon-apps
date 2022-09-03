ef6a247a4d1eb8b3cd2e2458ed63d26586639d2c
smrm-dev <mrmousavi78@gmail.com>
2022-09-03 11:37:45 +0430

    Use `isPriceToleranceOk` function of `price_feed` app Change `tokenAddress` into `token`

--- general/token_price_feed.js
+++ general/token_price_feed.js
@@ -1,4 +1,5 @@
 const { toBaseUnit, soliditySha3, BN, ethCall } = MuonAppUtils
+const { isPriceToleranceOk } = require('./price_feed')
 
 const CHAINS = {
     mainnet: 1,
@@ -25,44 +26,28 @@ module.exports = {
     REMOTE_CALL_TIMEOUT: 30000,
 
 
-    isPriceToleranceOk: function (price, expectedPrice, priceTolerance) {
-        let priceDiff = new BN(price).sub(new BN(expectedPrice)).abs()
-
-        if (
-            new BN(priceDiff).mul(toBaseUnit('1', '18'))
-                .div(new BN(expectedPrice))
-                .gt(toBaseUnit(priceTolerance, '18'))
-        ) {
-            return false
-        }
-        return true
+    getRoute: function (chainId, token) {
+        return ROUTES[chainId][token]
     },
 
-    getRoute: function (chainId, tokenAddress) {
-        return ROUTES[chainId][tokenAddress]
-    },
-
-    getTokenPairPrice: async function (chain, pairAddress, tokenAddress) {
+    getTokenPairPrice: async function (chain, pairAddress, token) {
         let request = {
             method: 'signature',
             data: {
-                params: {
-                    chain: chain,
-                    pairAddress: pairAddress,
-                }
+                params: { chain, pairAddress }
             }
         }
 
         let pairPrice = await this.invoke("price_feed", "onRequest", request)
         let token0 = await ethCall(pairAddress, 'token0', [], UNISWAPV2_PAIR_ABI, CHAINS[chain])
         let token1 = await ethCall(pairAddress, 'token1', [], UNISWAPV2_PAIR_ABI, CHAINS[chain])
-        if (tokenAddress == token0) return { price: new BN(pairPrice.price0), token: token1 }
+        if (token == token0) return { price: new BN(pairPrice.price0), token: token1 }
         return { price: new BN(pairPrice.price1), unitToken: token0 }
     },
 
-    calculatePrice: async function (chain, route, tokenAddress) {
+    calculatePrice: async function (chain, route, token) {
         let price = Q112
-        let tokenPairPrice = { unitToken: tokenAddress }
+        let tokenPairPrice = { unitToken: token }
         for (var pairAddress of route) {
             tokenPairPrice = await this.getTokenPairPrice(chain, pairAddress, tokenPairPrice.unitToken)
             price = price.mul(tokenPairPrice.price).div(Q112)
@@ -79,20 +64,20 @@ module.exports = {
         switch (method) {
             case 'signature':
 
-                let { chain, tokenAddress } = params
+                let { chain, token } = params
                 if (!chain) throw { message: 'Invalid chain' }
 
                 const chainId = CHAINS[chain]
 
                 // get token route for calculating price
-                const route = this.getRoute(chainId, tokenAddress)
+                const route = this.getRoute(chainId, token)
                 if (!route) throw { message: 'Invalid token' }
                 // calculate price using the given route
-                const price = await this.calculatePrice(chain, route, tokenAddress)
+                const price = await this.calculatePrice(chain, route, token)
 
                 return {
                     chain: chain,
-                    tokenAddress: tokenAddress,
+                    token: token,
                     route: route,
                     price: price.toString()
                 }
@@ -110,15 +95,15 @@ module.exports = {
         switch (method) {
             case 'signature': {
 
-                let { chain, tokenAddress, route, price } = result
+                let { chain, token, route, price } = result
 
                 const expectedPrice = request.data.result.price
 
-                if (!this.isPriceToleranceOk(price, expectedPrice, PRICE_TOLERANCE)) throw { message: 'Price threshold exceeded' }
+                if (!isPriceToleranceOk(price, expectedPrice, PRICE_TOLERANCE).isOk) throw { message: 'Price threshold exceeded' }
 
                 return soliditySha3([
                     { type: 'uint32', value: this.APP_ID },
-                    { type: 'address', value: tokenAddress },
+                    { type: 'address', value: token },
                     { type: 'address[]', value: route },
                     { type: 'uint256', value: expectedPrice },
                     { type: 'uint256', value: String(CHAINS[chain]) },
