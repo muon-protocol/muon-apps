93065bb9817f945a26d2598e4de308efa466e02a
smrm-dev <mrmousavi78@gmail.com>
2022-09-04 18:30:48 +0430

    Extend `token_price_feed` app with `price_feed` app Use `caculatePrice` instead of `invoke` Remove `isPriceToleranceOk`

--- general/token_price_feed.js
+++ general/token_price_feed.js
@@ -1,10 +1,11 @@
 const { soliditySha3, BN } = MuonAppUtils
-const { isPriceToleranceOk } = require('./price_feed')
+const PriceFeed = require('./price_feed')
 
-const CHAINS = {
-    mainnet: 1,
-    fantom: 250,
-}
+const {
+    CHAINS,
+    networksWeb3,
+    Q112
+} = PriceFeed
 
 const ROUTES = {
     [CHAINS.mainnet]: {
@@ -21,11 +22,10 @@ const ROUTES = {
     }
 }
 
-const PRICE_TOLERANCE = '0.0005'
-const Q112 = new BN(2).pow(new BN(112))
-
 
 module.exports = {
+    ...PriceFeed,
+
     APP_NAME: 'token_price_feed',
     APP_ID: 100,
     REMOTE_CALL_TIMEOUT: 30000,
@@ -35,25 +35,18 @@ module.exports = {
         return ROUTES[chainId][token]
     },
 
-    getTokenPairPrice: async function (chain, pairAddress, reversed) {
-        let request = {
-            method: 'signature',
-            data: {
-                params: { chain, pairAddress }
-            }
-        }
-
-        let pairPrice = await this.invoke("price_feed", "onRequest", request)
+    getTokenPairPrice: async function (chainId, pairAddress, reversed, toBlock) {
+        let pairPrice = await this.calculatePairPrice(chainId, pairAddress, toBlock)
         return new BN(reversed ? new BN(pairPrice.price1) : new BN(pairPrice.price0))
     },
 
-    calculatePrice: async function (chain, route) {
+    calculatePrice: async function (chainId, route, toBlock) {
         let price = Q112
         let tokenPairPrice
         var zip = (a, b) => a.map((x, i) => [x, b[i]]);
 
         for (let [pairAddress, reversed] of zip(route.route, route.reversed)) {
-            tokenPairPrice = await this.getTokenPairPrice(chain, pairAddress, reversed)
+            tokenPairPrice = await this.getTokenPairPrice(chainId, pairAddress, reversed, toBlock)
             price = price.mul(tokenPairPrice).div(Q112)
         }
         return price
@@ -72,18 +65,23 @@ module.exports = {
                 if (!chain) throw { message: 'Invalid chain' }
 
                 const chainId = CHAINS[chain]
+                const w3 = networksWeb3[chainId]
+                let toBlock
+                if (!request.data.result) toBlock = await w3.eth.getBlockNumber()
+                else toBlock = request.data.result.toBlock
 
                 // get token route for calculating price
                 const route = this.getRoute(chainId, token)
                 if (!route) throw { message: 'Invalid token' }
                 // calculate price using the given route
-                const price = await this.calculatePrice(chain, route)
+                const price = await this.calculatePrice(chainId, route, toBlock)
 
                 return {
                     chain: chain,
                     token: token,
                     route: route.route,
-                    price: price.toString()
+                    price: price.toString(),
+                    toBlock: toBlock
                 }
 
             default:
@@ -101,15 +99,11 @@ module.exports = {
 
                 let { chain, token, route, price } = result
 
-                const expectedPrice = request.data.result.price
-
-                if (!isPriceToleranceOk(price, expectedPrice, PRICE_TOLERANCE).isOk) throw { message: 'Price threshold exceeded' }
-
                 return soliditySha3([
                     { type: 'uint32', value: this.APP_ID },
                     { type: 'address', value: token },
                     { type: 'address[]', value: route },
-                    { type: 'uint256', value: expectedPrice },
+                    { type: 'uint256', value: price },
                     { type: 'uint256', value: String(CHAINS[chain]) },
                     { type: 'uint256', value: request.data.timestamp }
                 ])
