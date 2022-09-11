7c8a80b1cbba4c12e4a8eee6348f81cdbaca5469
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-09-12 03:10:14 +0430

    fear_price updates

--- general/fear_price.js
+++ general/fear_price.js
@@ -1,9 +1,11 @@
 const { axios, soliditySha3, floatToBN } = MuonAppUtils
 
-function getPrice() {
+const PRICE_TOLERANCE = '0.05';
+
+async function callAPI(method) {
   return axios
     .get(
-      'https://api.fear.io/api/cp'
+      `https://api.fear.io/api/${method}`
     )
     .then(({ data }) => data)
     .catch((err) => {
@@ -21,14 +23,22 @@ module.exports = {
     } = request;
     switch (method) {
       case 'get_price':
-        let result = await getPrice();
+        let [fearPrice, ethPrice] = await Promise.all([
+          callAPI('cpg'),
+          callAPI('cpge')
+        ]);
 
-        if (!result || result == 0) {
-          throw { message: 'invalid price' }
+        if (!fearPrice || fearPrice == 0 || isNaN(fearPrice)) {
+          throw { message: 'invalid fear price' }
+        }
+
+        if (!ethPrice || ethPrice == 0 || isNaN(ethPrice)) {
+          throw { message: 'invalid eth price' }
         }
 
         return {
-          price: floatToBN(result, 18).toString()
+          fearPrice: floatToBN(fearPrice, 18).toString(),
+          ethPrice: floatToBN(ethPrice, 18).toString()
         }
 
       default:
@@ -40,15 +50,50 @@ module.exports = {
     let { method } = request
     switch (method) {
       case 'get_price':
-        let { price } = result
+        let { fearPrice, ethPrice } = result;
+        if (
+          !this.isPriceToleranceOk(
+            fearPrice,
+            request.data.result.fearPrice
+          )
+        ) {
+          throw { message: 'fearPrice difference is not acceptable.' }
+        }
+
+        if (
+          !this.isPriceToleranceOk(
+            ethPrice,
+            request.data.result.ethPrice
+          )
+        ) {
+          throw { message: 'ethPrice difference is not acceptable.' }
+        }
+
         return soliditySha3([
           { type: 'uint256', value: this.APP_ID },
-          { type: 'uint256', value: request.data.result.price },
+          { type: 'uint256', value: request.data.result.fearPrice },
+          { type: 'uint256', value: request.data.result.ethPrice },
           { type: 'uint256', value: request.data.timestamp}
         ])
 
       default:
         break
     }
+  },
+
+  // price: calculated price on the current node
+  // expectedPrice: this price came from the gateway node
+  // and the current node wants to sign it
+  isPriceToleranceOk: function (price, expectedPrice) {
+    let priceDiff = new BN(price).sub(new BN(expectedPrice)).abs()
+
+    if (
+      new BN(priceDiff)
+        .div(new BN(expectedPrice))
+        .gt(toBaseUnit(PRICE_TOLERANCE, '18'))
+    ) {
+      return false
+    }
+    return true
   }
 }
