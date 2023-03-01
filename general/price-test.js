d949af16e41ef43d7f95bdda7b80efec78cb01ec
farazfo@yahoo.com <11iloveu005>
2023-03-01 18:42:01 +0330

    test app price

---
+++ general/price-test.js
@@ -0,0 +1,37 @@
+const {axios} = MuonAppUtils;
+
+module.exports = {
+    APP_NAME: 'price_test',
+
+    onRequest: async function (request) {
+        let {method, data: {params}} = request;
+        switch (method) {
+            case 'get_price':
+                const currencyName = params.currency;
+                const response = await axios
+                    .get(`https://api.coingecko.com/api/v3/simple/price?ids=${currencyName}&vs_currencies=usd`);
+                let price = response.data;
+                if (price[currencyName])
+                    price = parseInt(price[currencyName]["usd"] * 1000000);
+                else
+                    throw `get price failed`;
+                return {price};
+
+            default:
+                throw `Unknown method ${method}`
+        }
+    },
+
+    signParams: function (request, result) {
+        let {method} = request;
+        let {price} = result;
+        switch (method) {
+            case 'get_price':
+                return [
+                    {type: 'uint32', value: price}
+                ];
+            default:
+                throw `Unknown method '${method}'`
+        }
+    }
+};
