90576eb1c136ff7b5a2972e5e8e0bf01893d854f
smrm-dev <smrmdev@gmail.com>
2023-08-16 15:08:37 +0330

    Implement `getEntryDetail`

--- general/unitap.js
+++ general/unitap.js
@@ -1,8 +1,28 @@
+const { axios } = MuonAppUtils
+
 const UnitapApp = {
     APP_NAME: 'unitap',
 
     getEntryDetail: async function (raffleEntryId) {
+        const url = `https://stage.unitap.app/api/prizetap/raffle-enrollment/detail/${raffleEntryId}/`
+        let result
+        try {
+            result = await axios.get(url)
+        }
 
+        catch (e) {
+            throw e.response.data
+        }
+
+        const { chain, wallet, multiplier, raffle } = result.data.entry
+        const raffleId = raffle.raffleId
+
+        return {
+            chain,
+            wallet,
+            raffleId,
+            multiplier,
+        }
     },
 
     onRequest: async function (request) {
