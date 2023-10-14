2a8c275e54232496c0397a51fa234f978f08e88e
smrm-dev <smrmdev@gmail.com>
2023-10-14 15:06:28 +0330

    Add `random-words` method to `stage_unitap` app

--- general/stage_unitap.js
+++ general/stage_unitap.js
@@ -1,4 +1,7 @@
-const { axios } = MuonAppUtils
+const { axios, ethCall } = MuonAppUtils
+
+const PRIZE_TAP_VRF_CLIENT = "0xdf60b75E3974BBFD60CF0d7e05e09C9CdddC0994"
+const PRIZE_TAP_VRF_CLIENT_ABI = [{ "inputs": [{ "internalType": "uint256", "name": "requestId", "type": "uint256" }], "name": "getRandomWords", "outputs": [{ "internalType": "uint256[]", "name": "", "type": "uint256[]" }], "stateMutability": "view", "type": "function" }]
 
 const StageUnitapApp = {
     APP_NAME: 'stage_unitap',
@@ -30,6 +33,16 @@ const StageUnitapApp = {
         else throw { detail: 'CORRUPTED_ENTRY' }
     },
 
+    getRandomWords: async function (requestId) {
+        const randomWords = await ethCall(PRIZE_TAP_VRF_CLIENT, 'getRandomWords', [requestId], PRIZE_TAP_VRF_CLIENT_ABI, 80001)
+        if (randomWords.length == 0) {
+            throw {
+                detail: 'NO_RECORD_FOUND',
+            }
+        }
+        return { randomWords }
+    },
+
     onRequest: async function (request) {
         let { method, data: { params } } = request;
         switch (method) {
@@ -52,6 +65,18 @@ const StageUnitapApp = {
                     wallet,
                     raffleId,
                     multiplier,
+                }
+
+            case 'random-words':
+                let {
+                    requestId,
+                } = params
+
+                const { randomWords } = await this.getRandomWords(requestId)
+
+                return {
+                    requestId,
+                    randomWords,
                 }
 
             default:
@@ -79,6 +104,18 @@ const StageUnitapApp = {
                     { type: 'uint256', value: multiplier },
                 ]
             }
+
+            case 'random-words':
+                let {
+                    requestId,
+                    randomWords,
+                } = result
+
+
+                return [
+                    { type: 'uint256', value: requestId },
+                    { type: 'uint256[]', value: randomWords },
+                ]
 
             default:
                 throw { message: `Unknown method: ${request.method}` }
