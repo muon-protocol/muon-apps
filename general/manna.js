aa206ec32b3592f8d19e27fb1908710ee132eca0
Hassan Abbasi <hassan.abbp@gmail.com>
2023-10-22 22:12:36 +0330

    Add caching to gitcoin score

--- general/manna.js
+++ general/manna.js
@@ -7,43 +7,58 @@ function verifySignedMessage(message, signature, expectedAddress) {
   return recoveredAddress.toLowerCase() === expectedAddress.toLowerCase();
 }
 
-const scorer_id = process.env.SCORER_ID;
-const api_key = process.env.API_KEY;
-const scorer_url = "https://api.scorer.gitcoin.co/registry/submit-passport"
+const scorerId = process.env.SCORER_ID;
+const apiKey = process.env.API_KEY;
+const scorerUrl = "https://api.scorer.gitcoin.co/registry/submit-passport"
+const checkInMessagePrefix = "Check-in timestamp: "
+const gitcoinPassportMessagePrefix = "Verification request timestamp: "
+const redisPrefix = "manna-gitcoin-score-"
 
 const MannaApp = {
   APP_NAME: 'manna',
+  dependencies: ['redis'],
   useTss: true,
 
   onRequest: async function (request) {
     let {method, data: {params = {}}} = request;
     let {signature, timestamp, address} = params;
 
-    const timeNow = Date.now() / 1000;
+    const timeNow = Math.floor(Date.now() / 1000);
     if (timeNow > timestamp + (60 * 3) || timestamp > timeNow)
       throw {message: "invalid timestamp"}
 
     switch (method) {
       case 'checkIn':
-        if (!verifySignedMessage(timestamp.toString(), signature, address))
+        if (!verifySignedMessage(checkInMessagePrefix + timestamp.toString(), signature, address))
           throw {message: "invalid signature"}
         return {timestamp, address};
 
       case 'gitcoinScore':
+        if (!verifySignedMessage(gitcoinPassportMessagePrefix + timestamp.toString(), signature, address))
+          throw {message: "invalid signature"}
+        let cache = await this.redis.get(redisPrefix + address);
+        if (cache != null) {
+          let hitCache = JSON.parse(cache);
+          if (timeNow <= hitCache.timestamp + (60 * 15)) {
+            const score = hitCache.score;
+            return {score, timestamp, address};
+          }
+        }
         const config = {
           headers: {
             'Content-Type': 'application/json',
-            'X-API-KEY': api_key
+            'X-API-KEY': apiKey
           }
         };
         const params = {
           'address': address,
-          'scorer_id': scorer_id
+          'scorer_id': scorerId
         };
-        const data = (await axios.post(scorer_url, params, config)).data;
+        const data = (await axios.post(scorerUrl, params, config)).data;
         if (data.score == null)
           throw {message: "We have relieved rate limit error from gitcoin. Please try again later"}
         let score = Math.floor(data.score * 10 ** 6);
+        await this.redis.set(redisPrefix + address, JSON.stringify({timestamp: timeNow, score: score}));
         return {score, timestamp, address};
 
       case 'test':
