675e82e55d92ec1f87396a662e660a2a077043e9
smrm-dev <smrmdev@gmail.com>
2023-12-09 18:24:50 +0330

    Implement `askGPT`

--- general/chatGPT.js
+++ general/chatGPT.js
@@ -1,5 +1,36 @@
+const { OpenAI } = require("openai");
+
+const openai = new OpenAI({ apiKey: process.env.GPT_API_KEY });
+
 const ChatGPTApp = {
     APP_NAME: 'chatGPT',
+
+    askGPT: async function (question) {
+        try {
+            const completion = await openai.chat.completions.create({
+
+                messages: [
+                    { role: "system", content: "Answer with true or false." },
+                    { role: "user", content: question }
+                ],
+                model: "gpt-3.5-turbo",
+            });
+
+            let answer = completion.choices[0].message.content;
+            if (!(["True.", "False.", "False", "True"].includes(answer))) {
+                throw { message: "GPT_NOT_ANSWERED_WITH_TRUE_OR_FALSE" }
+            }
+
+            answer = answer == "True." ? true : false;
+
+            return answer
+        }
+        catch (e) {
+            console.log(e)
+            throw { message: "FAILED_TO_REACH_GPT" }
+        }
+    },
+
     onRequest: async function (request) {
         let { method, data: { params } } = request;
         switch (method) {
